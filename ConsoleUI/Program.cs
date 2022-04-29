3b9b9a54da5ee826089439e84fdf28e74d1267f4
Adithya Vijay K <adithyavj@gmail.com>
2022-04-29 12:29:07 +0530

    Add logic in ConsoleUI

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
+using DemoLibrary;
+    
 namespace ConsoleUI
 {
     class Program
     {
+        static ShoppingCartModel cart = new ShoppingCartModel();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PopulateCartWithDemoData();
+
+            Console.WriteLine($"The total for the cart is {cart.GenerateTotal():C2}");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit the application");
+            Console.ReadLine();
+        }
+
+        public static void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.51M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.84M });
         }
     }
 }
