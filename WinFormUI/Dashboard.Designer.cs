c3ce23f3408e0a81459ebc2bab996a6b95525c95
Adithya Vijay K <adithyavj@gmail.com>
2022-05-10 13:20:27 +0530

    Using same delegate in winformUI. - Delegate can be used in different applications and have them do different things and logic in the base library doesn't have to change at all. - Delegates are used a lot in events.

--- WinFormUI/Dashboard.Designer.cs
+++ WinFormUI/Dashboard.Designer.cs
@@ -29,12 +29,96 @@ namespace WinFormUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_msgbox = new System.Windows.Forms.Button();
+            this.btn_txtbox = new System.Windows.Forms.Button();
+            this.txt_subtotal = new System.Windows.Forms.TextBox();
+            this.lbl_subtotal = new System.Windows.Forms.Label();
+            this.lbl_total = new System.Windows.Forms.Label();
+            this.txt_total = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_msgbox
+            // 
+            this.btn_msgbox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_msgbox.Location = new System.Drawing.Point(36, 172);
+            this.btn_msgbox.Name = "btn_msgbox";
+            this.btn_msgbox.Size = new System.Drawing.Size(209, 81);
+            this.btn_msgbox.TabIndex = 0;
+            this.btn_msgbox.Text = "MessageBox Demo";
+            this.btn_msgbox.UseVisualStyleBackColor = true;
+            this.btn_msgbox.Click += new System.EventHandler(this.btn_msgbox_Click);
+            // 
+            // btn_txtbox
+            // 
+            this.btn_txtbox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_txtbox.Location = new System.Drawing.Point(253, 172);
+            this.btn_txtbox.Name = "btn_txtbox";
+            this.btn_txtbox.Size = new System.Drawing.Size(209, 81);
+            this.btn_txtbox.TabIndex = 1;
+            this.btn_txtbox.Text = "TextBox Demo";
+            this.btn_txtbox.UseVisualStyleBackColor = true;
+            this.btn_txtbox.Click += new System.EventHandler(this.btn_txtbox_Click);
+            // 
+            // txt_subtotal
+            // 
+            this.txt_subtotal.Location = new System.Drawing.Point(258, 54);
+            this.txt_subtotal.Name = "txt_subtotal";
+            this.txt_subtotal.Size = new System.Drawing.Size(209, 23);
+            this.txt_subtotal.TabIndex = 2;
+            // 
+            // lbl_subtotal
+            // 
+            this.lbl_subtotal.AutoSize = true;
+            this.lbl_subtotal.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_subtotal.Location = new System.Drawing.Point(258, 22);
+            this.lbl_subtotal.Name = "lbl_subtotal";
+            this.lbl_subtotal.Size = new System.Drawing.Size(73, 21);
+            this.lbl_subtotal.TabIndex = 3;
+            this.lbl_subtotal.Text = "Sub Total";
+            // 
+            // lbl_total
+            // 
+            this.lbl_total.AutoSize = true;
+            this.lbl_total.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_total.Location = new System.Drawing.Point(258, 84);
+            this.lbl_total.Name = "lbl_total";
+            this.lbl_total.Size = new System.Drawing.Size(42, 21);
+            this.lbl_total.TabIndex = 5;
+            this.lbl_total.Text = "Total";
+            // 
+            // txt_total
+            // 
+            this.txt_total.Location = new System.Drawing.Point(258, 114);
+            this.txt_total.Name = "txt_total";
+            this.txt_total.Size = new System.Drawing.Size(209, 23);
+            this.txt_total.TabIndex = 4;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(528, 283);
+            this.Controls.Add(this.lbl_total);
+            this.Controls.Add(this.txt_total);
+            this.Controls.Add(this.lbl_subtotal);
+            this.Controls.Add(this.txt_subtotal);
+            this.Controls.Add(this.btn_txtbox);
+            this.Controls.Add(this.btn_msgbox);
+            this.Name = "Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dashboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_msgbox;
+        private System.Windows.Forms.Button btn_txtbox;
+        private System.Windows.Forms.TextBox txt_subtotal;
+        private System.Windows.Forms.Label lbl_subtotal;
+        private System.Windows.Forms.Label lbl_total;
+        private System.Windows.Forms.TextBox txt_total;
     }
 }
--- WinFormUI/Dashboard.cs
+++ WinFormUI/Dashboard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,49 @@ namespace WinFormUI
 {
     public partial class Dashboard : Form
     {
+        ShoppingCartModel cart = new ShoppingCartModel();
+
         public Dashboard()
         {
             InitializeComponent();
+            PopulateCartWithDemoData();
+        }
+
+        private void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.51M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.84M });
+        }
+
+        private void btn_msgbox_Click(object sender, EventArgs e)
+        {
+            decimal total = cart.GenerateTotal(SubTotalAlert, CalculateLeveledDiscount, PrintOutDiscountAlert);
+            MessageBox.Show($"The total is {total:C2}");
+        }
+
+        private void btn_txtbox_Click(object sender, EventArgs e)
+        {
+            decimal total = cart.GenerateTotal((subTotal) => txt_subtotal.Text = $"{subTotal:C2}",
+                (products, subTotal) => subTotal - products.Count * 2,
+                (message) => { });
+            txt_total.Text = $"{total:C2}";
+        }
+
+        private void PrintOutDiscountAlert(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        private void SubTotalAlert(decimal subTotal)
+        {
+            MessageBox.Show($"The subtotal is {subTotal:C2}");
+        }
+
+        private decimal CalculateLeveledDiscount(List<ProductModel> products, decimal subTotal)
+        {
+            return subTotal - products.Count();
         }
     }
 }
